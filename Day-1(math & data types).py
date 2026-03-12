a1675ba029a12216b6d5bc840c03c4d29406ac36
Eddie <eddiemtk@gmail.com>
2026-03-12 21:52:31 +0000

    Finished day 1

---
+++ Day-1(math & data types).py
@@ -0,0 +1,20 @@
+print('hello world')
+print(5 + 1) #Addition
+print(5 - 1) #subtraction
+print(5 / 1.1) #division
+print(5 * 1) #multiplication
+print(5 % 2) #modulous, what remains after i do how many times 2 goes into 5, thats 2 times then 1 is left, so 1 is the answer
+print(5 ** 2) #exponetial
+print(5 // 1.1) #floor division returns the largest integer that is less than or equal to the result of the division
+
+
+print(type(11)) #integer (int)
+print(type(1.1)) #float (float)
+print(type('Arnold')) #string (str)
+print(type(1 + 3j)) #complex (complex)
+print(type([1,2,3])) #List
+print(type({'name' : 'Shanks'})) #dictionary (dict)
+print(type({9.2, 2.2, 4.8})) #set
+print(type((9.8, 3.14, 2.7))) # Tuple
+#Difference between a set and a tuple is set uses {}, while tuple uses ()
+#Another difference is 
