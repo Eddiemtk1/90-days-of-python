5c24bf6d6f64b99a099273722c7a725ce55e5aad
Eddie <eddiemtk@gmail.com>
2026-03-13 21:15:54 +0000

    started day 3 practice questions stopped at question 7

---
+++ day-3(practice questions).py
@@ -0,0 +1,23 @@
+age = 21
+my_height = 175.0
+complicated = 1 + 3j
+
+# base = float(input('Enter base: '))
+# height = float(input('Enter height: '))
+# #You can't multiply a float and an integer,
+# area = 0.5 * base * height
+# print(f'The area of the triangle = {area}!')
+
+
+# side_a = int(input('Enter side a: '))
+# side_b = int(input('Enter side b: '))
+# side_c = int(input('Enter side c: '))
+# total_perimeter = side_a + side_b + side_c
+# print(f'the total perimeter of the triangle is {total_perimeter}!!!')
+
+length = int(input('Enter the length: '))
+width = int(input('Enter the width: '))
+area = length * width
+print(f'area = {area}')
+perimeter = 2 * (length * width)
+print(f'Perimeter is {perimeter}')
