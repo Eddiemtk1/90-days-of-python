5610d1bb2d6f0fdb2d0751415920efdd9d637081
Eddie <eddiemtk@gmail.com>
2026-03-13 20:58:24 +0000

    made notes for day 3

---
+++ day-3(operations).py
@@ -0,0 +1,24 @@
+#Assigment operators
+
+
+
+#Comparison operators
+# == is equal
+# != is not equal
+# < is greater than
+# > is less than
+# <= is equal to or greater than
+# >= is equal to or less than
+
+# 'is' returns true if both variables are the same object e.g print('1 is 1', 1 is 1)
+# 'is not' returns true if both variables are NOT the same object e.g. print('1 is not 2', 1 is not 2)
+# 'in' returns true if the sorted list has a certain item e.g print('A in Asabeneh', 'A' in 'Asabeneh')
+# 'not in' returns true if the sorted list does not have a certain item e.g. print('B not in Asabeneh', 'B' in 'Asabeneh')
+
+
+#logical operators
+# and e.g. print(3 > 2 and 4 > 3) # True - because both statements are true
+# or e.g. print(3 > 2 or 4 > 3)  # True - because both statements are true
+# not e.g. print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+
+
