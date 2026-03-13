99417b40d223dd494d82b7c6cd28865805a3b75b
Eddie <eddiemtk@gmail.com>
2026-03-13 12:46:08 +0000

    learned about variables

--- day-2(built in functions).py
+++ day-2(built in functions).py
@@ -23,3 +23,44 @@ print(min([20, 15, 0.17, 0.00002])) #takes the list as an argument then gives th
 print(max([20, 15, 0.17, 0.00002])) #takes the list as an argument then gives the maximum value
 
 
+
+#Variables
+#variables can be stored on multiple lines
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
+
+
+# Printing the values stored in the variables
+print('First name:', first_name), print('First name length:')
+print(len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+#variables can be stored on a single line: first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+#ctrl + / to make sections of code into comments
+# my_age = input('enter your name: ')
+# if my_age == '21':
+#     print('You are the one')
+# else:
+#     print('You are not the one')
+
+name = input('Whats your name: ')
+print('your name is: ', name)
