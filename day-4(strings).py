2242b9506bfb235372e7de6d5172d29bbcdd2a93
Eddie <eddiemtk@gmail.com>
2026-03-14 20:15:44 +0000

    took a break

--- day-4(strings).py
+++ day-4(strings).py
@@ -15,6 +15,11 @@
 # print('This is a backslash  symbol (\\)')
 # print("In every programming language it starts with \'Hello, World!\'") # to write a double quote inside a single quote
 
+#Old style string Formatting
+# %s - string
+# %d - integer
+# %f - floating point numbers
+# %.number of digitsf - Floating point numbers with fixed precision
 
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
@@ -22,3 +27,11 @@ language = 'Python'
 formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
 # formated_string = (f"I am {first_name} {last_name}. I teach {language}") My version
 print(formated_string)
+
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area) # 2 refers the 2 significant digits after the point
+
+#New style formatting
