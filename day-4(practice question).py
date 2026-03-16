e49d74af46b0b14128d84df0324682dec611aff9
Eddie <eddiemtk@gmail.com>
2026-03-16 16:00:26 +0000

    got to question 10 on practice questions

---
+++ day-4(practice question).py
@@ -0,0 +1,22 @@
+#join
+# list = ['Thirty', 'Days', 'Of', 'Python']
+# list_1 = ' '.join(list)
+# print(list_1)
+
+
+# list_1 = ['Coding', 'For' , 'All']
+# list_2 = ' '.join(list_1)
+# print(list_2)
+
+
+company = "Coding For All"
+# print(company)
+# print(len(company))
+# print(company.upper())
+# print(company.lower())
+
+# print(company.capitalize())
+# print(company.title())
+# print(company.swapcase())
+
+print(company.strip("Coding"))
