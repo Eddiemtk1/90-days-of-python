68b20d86614829d446c3e15e68d7e8ac67714839
Eddie <eddiemtk@gmail.com>
2026-03-16 15:46:02 +0000

    small changes, starting to make notes

--- day-4(strings).py
+++ day-4(strings).py
@@ -98,47 +98,47 @@
 #so we are left with h, l, o.
 
 #string methods
-#capitalize()
+###capitalize()
 # challenge = 'thirty days of coding python'
 # # # print(challenge.capitalize())
 
-# # #count() returns the number of non-overlapping times a substring appears in a string, starting and ending indexes are optional
+###count() returns the number of non-overlapping times a substring appears in a string, starting and ending indexes are optional
 # # print(challenge.count('o')) #3
 # # print(challenge.count('o',2,6)) #0 'how many 'o' between index 2 - 6
 
-# #endswitch() checks if a string ends in a specific ending
+###endswitch() checks if a string ends in a specific ending
 # print(challenge.endswith('on'))#true
 # print(challenge.endswith('days'))#false
 
-#expandtabs() replaces tab character with spaces, default tab size is 8 but actually 4 characters.
+###expandtabs() replaces tab character with spaces, default tab size is 8 but actually 4 characters.
 # challenge = 'thirty\tdays\tof\tpython'
 # print(challenge.expandtabs())
 # print(challenge.expandtabs(10))#thirty    days      of        python, Space 4,6,8
 
-#find()returns the index of the first occurence of a substring, if not found returns -1
+###find()returns the index of the first occurence of a substring, if not found returns -1
 # print(challenge.find('t')) #0
 # print(challenge.find('tz')) #-1
 
-# #rfind() find the index of the last occurence of a substring, if not found returns -1
+###rfind() find the index of the last occurence of a substring, if not found returns -1
 # print(challenge.rfind('t'))#17
 
-#format() formats string into a nicer output
+###format() formats string into a nicer output
 # formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
 # print(formated_string)
 
-#index()returns the lowest index of a substring, additional arguments indicate starting and ending index. 
+###index()returns the lowest index of a substring, additional arguments indicate starting and ending index. 
 #if the substring isn't found it returns a valueError
 # challenge = 'thirty days of python'
 # sub_string = 'da'
 # # print(challenge.index(sub_string)) #7
 # # print(challenge.index(sub_string,9))#Error
 
-# #rindex() returns the highest index of a substring, additional arguments indicate starting and ending index.
+###rindex() returns the highest index of a substring, additional arguments indicate starting and ending index.
 # print(challenge.rindex(sub_string)) #7
 # # print(challenge.rindex(sub_string, 9))
 # print(challenge.rindex('t',5))#19 #finds the highest index of 't' after index 5.
 
-#isalnum() chcks if the variable has only alphanumetric characters
+###isalnum() chcks if the variable has only alphanumetric characters
 # challenge = 'thirtyDays'
 # print(challenge.isalnum()) #true
 # challenge = '30Days'
@@ -146,13 +146,13 @@
 # challenge = 'thirty Days'
 # print(challenge.isalnum()) #False as space is not alphanumeric character
 
-#isalpha() checks if all string elements are alphabet characters doesnt matter if capitalised
+###isalpha() checks if all string elements are alphabet characters doesnt matter if capitalised
 # challenge = 'my name is'
 # print(challenge.isalpha()) #False because of spaces
 # challenge = 'mynameis'
 # print(challenge.isalpha()) #True
 
-#isdecimal() checks if all characters are decimal 0-9
+###isdecimal() checks if all characters are decimal 0-9
 # challenge = 'thirty days of python'
 # print(challenge.isdecimal()) #false
 # challenge_1 = '123'
@@ -162,14 +162,14 @@
 # print(challenge_2.isdigit()) #false
 
 
-#isdigit() Checks if all characters in a string are numbers (0-9 and some other unicode characters for numbers)
+###isdigit() Checks if all characters in a string are numbers (0-9 and some other unicode characters for numbers)
 # challenge_1= '\u00b2'
 # print(challenge_1.isdigit()) #true
 # challenge_2 = '30'
 # print(challenge_2.isdigit())#true
 
 
-#isnumeric() checks if all characters in a string are number or number related, also accepts symbols like ½
+###isnumeric() checks if all characters in a string are number or number related, also accepts symbols like ½
 # number = '10'
 # print(number.isnumeric()) #true 
 # number_1 = '\u00BD'
@@ -177,20 +177,20 @@
 # number_2 = '10.5'
 # print(number_2.isnumeric()) #false
 
-#isidentifier()checks if a string is a valid variable name
+###isidentifier()checks if a string is a valid variable name
 # problem = '30days of python'
 # print(problem.isidentifier()) #false as variables can't start with number and there's spaces
 # proble_1 = 'thirty_days'
 # print(proble_1.isidentifier()) #true
 
 
-#islower()checks if all alphabet characters in the string are lowercase
+###islower()checks if all alphabet characters in the string are lowercase
 # challenge = 'i am so tired'
 # print(challenge.islower()) #true
 # challenge_1 = 'I AM so tired'
 # print(challenge_1.islower()) #false
 
-#isupper() checks if all alphabet characters in the string are uppercase
+###isupper() checks if all alphabet characters in the string are uppercase
 #numbers and spaces don't count and will return true
 # challenge = 'I NEED SOME SLEEP'
 # print(challenge.isupper())#true
@@ -198,23 +198,23 @@
 # print(challenge_1.isupper())#true
 
 
-#join() returns a concatenated string
+###join() returns a concatenated string
 # list_uni = ['havard','oxford','yale','cambridge']
 # result_uni = ' . '.join(list_uni)
 # print(result_uni) # havard . oxford . yale . cambridge
 
 
-#strip() removes all given chracters starting from the beginning and end of the string
+###strip() removes all given chracters starting from the beginning and end of the string
 #removes all the 'n''o''t''h' in the variable.
 # challenge = 'thirty days of pythoonnn'
 # print(challenge.strip('noth')) # 'irty days of py'
 
 
-# #replace() replaces substring with a given string
+###replace() replaces substring with a given string
 # challenge = 'thirty days of python'
 # print(challenge.replace('python', 'coding')) # 'thirty days of coding'
 
-#split()
+###split()
 # challenge_1 = 'thirty days of python'
 # print(challenge_1.split()) #['thirty', 'days', 'of', 'python']
 
@@ -222,18 +222,18 @@
 # print(challenge_2.split(', ')) #['thirty', ' days', ' of', ' python']
 
 
-#title() returns a title cased string
+###title() returns a title cased string
 #if its already capital nothing changes
 # challenge = 'thirty days of code'
 # print(challenge.title())
 
-#swapcase() converts all uppercase to lowercase and all lower case to upper case
+##swapcase() converts all uppercase to lowercase and all lower case to upper case
 # challenge_11 = 'my name is eddie'
 # print(challenge_11.swapcase()) #MY NAME IS EDDIE
 
 # challenge_12 = 'MY naMe is EdDie'
 # print(challenge_12.swapcase()) #my NAmE IS eDdIE
 
-#startswith() checks if the string stars with a specified string
+###startswith() checks if the string stars with a specified string
 # text_1 = 'suffer, as i have'
 # print(text_1.startswith('suffer')) #True
