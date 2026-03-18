17e98cc0a8c0841e864a9de0a759ac067534b798
Eddie <eddiemtk@gmail.com>
2026-03-18 21:49:46 +0000

    started day 5

---
+++ day-5(Lists).py
@@ -0,0 +1,12 @@
+#Theres 4 collection data types in python: list, dictionary, tuple, set
+# List - A collection which is ordered and changeable, allows duplicate members
+# Tuple - A collection which is ordered and unchangeable, allows duplicate members
+# Set - A collection which is unordered, un-indexed and unmodifiable, but we can add new items to the set. Duplicate members are not allowed.
+# Dictionary - A collection which is unordered, changeable(modifiable) and indexed. No duplicate members.
+
+
+##How to make a list
+# list function: 1st = list()
+
+# square brackets 1st = []
+
