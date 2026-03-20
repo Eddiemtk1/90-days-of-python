a15af8f2909c99a5b53e843824313e46a8011714
Eddie <eddiemtk@gmail.com>
2026-03-20 00:05:31 +0000

    got stuck

--- day-5(practice questions).py
+++ day-5(practice questions).py
@@ -7,3 +7,14 @@ print(len(list_1))
 
 split_list = list_1[::2]
 print(split_list)
+
+mixed_data_types = ['Eddie','21','175','single','birmingham']
+it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+
+print(it_companies)
+
+length = len(it_companies)
+print(f'Number of companies: {length}')
+
+split_companies = it_companies.index[::2]
+print(split_companies)
