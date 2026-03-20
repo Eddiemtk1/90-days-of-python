0a558ae0d7b2a8a4b6d855151486a85859c34a01
Eddie <eddiemtk@gmail.com>
2026-03-20 00:07:03 +0000

    fixed question 9

--- day-5(practice questions).py
+++ day-5(practice questions).py
@@ -1,12 +1,12 @@
 # empty_list = []
 # print(empty_list)
 
-list_1 = ['apple','peach','tomato','pineapple','mango']
+# list_1 = ['apple','peach','tomato','pineapple','mango']
 
-print(len(list_1))
+# print(len(list_1))
 
-split_list = list_1[::2]
-print(split_list)
+# split_list = list_1[::2]
+# print(split_list)
 
 mixed_data_types = ['Eddie','21','175','single','birmingham']
 it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
@@ -16,5 +16,5 @@ print(it_companies)
 length = len(it_companies)
 print(f'Number of companies: {length}')
 
-split_companies = it_companies.index[::2]
+split_companies = it_companies[::3]
 print(split_companies)
